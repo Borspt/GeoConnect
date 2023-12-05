b74d70c839645f23cc4311bac082d9e33c7534f1
Borspt <boris.petrenko@phystech.edu>
2023-12-05 12:39:04 +0300

    Initial commit

---
+++ geoconnect/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine Learning model to predict cashflow of WildBerries marketplace order issuing points',
+    author='Boris Petrenko',
+    license='MIT',
+)
